e4f3656b40e5877d0320c5d220d41680ba4619d2
YokeSan <yokesan.choong@gmail.com>
2020-11-01 11:14:23 +0800

    I add a fontawesome tutorial link

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 '''
 Tutorial: How to add font awesome icon into HTML URL: https://www.youtube.com/watch?v=nHKjsqw0zw8
+Another Font Awesome tutorial URL: https://www.youtube.com/watch?v=wiw68Y-hPlA
 FontAweSome: https://fontawesome.com/v4.7.0/icons/
 '''
 from project import app
